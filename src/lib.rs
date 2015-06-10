196469c790ab55391e0404cd16ea4ba6c4fb721b
Dino Wernli <dinowernli@gmail.com>
2015-06-10 23:58:38 +0200

    Add newtypes for Observation, Reward, and Action and use Reward in the Agent implementation.

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,4 @@
 pub mod agent;
 pub mod environment;
+pub mod types;
 pub mod random;
--- src/main.rs
+++ src/main.rs
@@ -1,61 +1,36 @@
 extern crate ai;
 
 use ai::agent::Agent;
-use ai::environment::Environment;
-use ai::environment::CoinFlip;
+use ai::environment::{CoinFlip, Environment};
 use ai::random::Random;
+use ai::types::{Action, Observation, Reward};
 
 fn main() {
-  // test_random();
-
   // Use one RNG to bootstrap the others so that we only have one magic seed constant.
   let mut rand1 = Box::new(Random::new(5761567));
-  let mut rand2 = Box::new(rand1.new_child());
+  let rand2 = Box::new(rand1.new_child());
 
   // Setup the agent and the environment.
   let mut environment = CoinFlip::new(rand1);
   let mut agent = Agent::new(environment.num_actions(), rand2);
 
   // Let the agent loose on the environment.
-  let n_cycles = 50;
+  let n_cycles = 10;
   println!("Starting simulation with {} cycles", n_cycles);
   for cycle in 0..n_cycles {
     let observation = environment.observation();
-    let reward = environment.reward();
+    let reward = Reward(environment.reward());
 
     agent.update(observation, reward);
     let action = agent.act();
     assert!(action >= 0 && action < environment.num_actions());
 
-    println!("Cycle: {}, [observation={}, reward={}], took action {}", 
+    println!("Cycle: {}, [observation={}, {:?}], took action {}", 
         cycle, observation, reward, action);
 
     environment.update(action);
   }
   
   // Report results.
-  println!("The average reward after {} rounds is {}", agent.age(), agent.average_reward());
-}
-
-/// Benchmarks our random number generator by sampling it and producing a histogram.
-fn test_random() {
-  let mut random = Random::new(8374586759);
-  let k = 10;    // Range of numbers.
-  let n = 1000;  // Number of iterations.
-
-  // Grab a total of n random numbers in [0, k-1] and build a histogram.
-  let mut accum = vec![0; k];
-  for _ in 0..n {
-    let x = random.next_modulo(10);
-    let index = x as usize;
-    accum[index] = accum[index] + 1;
-  }
-
-  // Normalize the histogram by dividing by n.
-  // TODO(dinowernli) Figure out how to use map.
-  let mut ratios = vec![0.0; k];
-  for i in 0..k {
-    ratios[i] = (accum[i] as f64) / (n as f64);
-  }
-  println!("Ratios: {:?}", ratios);
+  println!("The average reward after {} rounds is {:?}", agent.age(), agent.average_reward());
 }
