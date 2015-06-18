6ac08173e9ec9f57c5f494c0e9d821139160f36f
Dino Wernli <dinowernli@gmail.com>
2015-06-18 06:56:31 +0200

    Fix the first iteration of the main run loop which was previously trying to get results before taking the first action.

--- src/main.rs
+++ src/main.rs
@@ -17,21 +17,20 @@ fn main() {
   let mut environment = CoinFlip::new(rand1);
   let mut agent = Agent::new(environment.num_actions(), rand2);
 
-  // Let the agent loose on the environment.
+  // Let the agent interact with the environment.
   let n_cycles = 10;
   println!("Starting simulation with {} cycles", n_cycles);
   for cycle in 0..n_cycles {
-    let observation = environment.observation();
-    let reward = Reward(environment.reward());
-
-    agent.update(observation, reward);
     let action = agent.act();
     assert!(action >= 0 && action < environment.num_actions());
-
-    println!("Cycle: {}, [observation={}, {:?}], took action {}",
-        cycle, observation, reward, action);
-
     environment.update(action);
+
+    let observation = environment.observation();
+    let reward = Reward(environment.reward());
+    agent.update(observation, reward);
+
+    println!("Cycle: {}, [action={}, observation={}, {:?}]",
+        cycle, action, observation, reward);
   }
   
   // Report results.
