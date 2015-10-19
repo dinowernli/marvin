597bfab5750889d8bb287c984861828c6e2107d6
Dino Wernli <dinowernli@gmail.com>
2015-10-19 23:17:01 +0200

    Fix some comments in the random implementation.

--- src/random.rs
+++ src/random.rs
@@ -22,7 +22,7 @@
 
 use rand::{Rng, SeedableRng, StdRng};
 
-/// Basic random number generator. May be predictable.
+/// Basic random number generator (not necessarily cryptographically secure).
 pub trait Random {
   /// Returns a random number in the range [0, limit - 1].
   fn next_modulo(&mut self, limit: u64) -> u64;
@@ -37,20 +37,19 @@ impl RandomImpl {
   pub fn create(seed: usize) -> Self {
     let seed_slice: &[_] = &[seed as usize];
     return RandomImpl {
-      //generator: SeedableRng::from_seed(seed_slice),
       generator: SeedableRng::from_seed(seed_slice),
     };
-  }
-
-  /// Returns an arbitrary random number.
-  pub fn next(&mut self) -> u64 {
-    self.generator.gen::<u64>()
   }
 
   /// Returns a new random number generator seeded with the
   /// next random number produced by this generator.
   pub fn new_child(&mut self) -> Self {
     return RandomImpl::create(self.next() as usize);
+  }
+
+  /// Returns a random number.
+  fn next(&mut self) -> u64 {
+    self.generator.gen::<u64>()
   }
 }
 
